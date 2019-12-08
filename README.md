80c4c8ecd1bd7e8b75df77d21a2b3de41e4d645b
liangliang <liangsqrt@163.com>
2019-12-08 21:19:51 +0800

    多台电脑同步

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# zhiyuan
+# 尝试1
 
 ## Project setup
 ```
--- src/components/nav/eindex.vue
+++ src/components/nav/eindex.vue
@@ -1,33 +1,39 @@
 <template>
     <div>
         <el-row>
-            <el-col :span="8">
-                <div id="logo">
-                    <h1>
-                        <a href="index.html">xx公司</a>
-                    </h1>
-                </div>
-            </el-col>
-            <el-col :span="10">
-                <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
-                    <el-menu-item index="1">处理中心</el-menu-item>
-                    <el-submenu index="2">
-                        <template slot="title">我的工作台</template>
-                        <el-menu-item index="2-1">选项1</el-menu-item>
-                        <el-menu-item index="2-2">选项2</el-menu-item>
-                        <el-menu-item index="2-3">选项3</el-menu-item>
-                        <el-submenu index="2-4">
-                            <template slot="title">选项4</template>
-                            <el-menu-item index="2-4-1">选项1</el-menu-item>
-                            <el-menu-item index="2-4-2">选项2</el-menu-item>
-                            <el-menu-item index="2-4-3">选项3</el-menu-item>
-                        </el-submenu>
-                    </el-submenu>
-                    <el-menu-item index="3" disabled>消息中心</el-menu-item>
-                    <el-menu-item index="4"><a href="https://www.ele.me" target="_blank">订单管理</a></el-menu-item>
+                <el-menu
+                        :default-active="activeIndex"
+                        class="el-menu-demo"
+                        mode="horizontal"
+                        @select="handleSelect"
+                        background-color="#545c64"
+                >
+                    <el-col :span="8">
+                        <div class="id">
+                            <strong>xxcompanty</strong>
+                        </div>
+                    </el-col>
+                    <el-col :span="10">
+                        <el-row>
+                            <el-menu-item index="1">处理中心</el-menu-item>
+                            <el-submenu index="2">
+                                <template slot="title">我的工作台</template>
+                                <el-menu-item index="2-1">选项1</el-menu-item>
+                                <el-menu-item index="2-2">选项2</el-menu-item>
+                                <el-menu-item index="2-3">选项3</el-menu-item>
+                                <el-submenu index="2-4">
+                                    <template slot="title">选项4</template>
+                                    <el-menu-item index="2-4-1">选项1</el-menu-item>
+                                    <el-menu-item index="2-4-2">选项2</el-menu-item>
+                                    <el-menu-item index="2-4-3">选项3</el-menu-item>
+                                </el-submenu>
+                            </el-submenu>
+                            <el-menu-item index="3" disabled>消息中心</el-menu-item>
+                            <el-menu-item index="4"><a href="https://www.ele.me" target="_blank">订单管理</a></el-menu-item>
+                        </el-row>
+
+                    </el-col>
                 </el-menu>
-            </el-col>
-            <el-col :span="6"></el-col>
         </el-row>
 
 
